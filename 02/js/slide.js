d5ba4479260c5ffc2b6abe3dff98b1a90e44d234
kimwoory <wor0927@naver.com>
2020-07-31 09:57:18 +0900

    g
    g

---
+++ 02/js/slide.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var btn01 = $(".pop_img01");
+    var btn02 = $(".pop_img02");
+    btn01.click(function(e){
+        e.preventDefault();
+        $(".dscrb02").stop().slideUp();
+        $(".dscrb01").stop().slideToggle();
+    });
+    btn02.click(function(e){
+        e.preventDefault();
+        $(".dscrb01").stop().slideUp();
+        $(".dscrb02").stop().slideToggle();
+    });
+    
+});
