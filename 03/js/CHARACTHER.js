ce16e815609e461a527fda22a0c09810e326eed1
kimwoory <wor0927@naver.com>
2020-08-04 12:06:28 +0900

    f
    fg

--- 03/js/CHARACTHER.js
+++ 03/js/CHARACTHER.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
           $("footer").load("foot.html");
 			
 			new WOW().init();
+			AOS.init();
         
         });
 
@@ -18,104 +19,103 @@ $(document).ready(function() {
 
 
 
-//---------------설국열차---------------//
-$(document).ready(function() {
-    $(window).scroll( function(){
-		    var Snowpiercer = $(".Snowpiercer").offset().top;	//설국열차 박스 탑값
-            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
-			
-			$('.container').stop().animate({opacity:"1"});
-			$('.hideme').addClass('SHOWME');
-
-			console.log("시작");
-			console.log("★설국열차.offset().top : " + $('.Snowpiercer').offset().top);	//430
-			console.log("$(window).scrollTop() : " +  bottom_of_window);
-			console.log("끝");
-		
-            if( bottom_of_window <= (Snowpiercer-230)){
-				// 스노우 박스(200) 사라짐
-               	$('.Snow-txt').stop().animate({opacity:"0"});
-				$('.Snow-img').stop().animate({opacity:"0"});
-            }
-		
-			if( bottom_of_window >= (Snowpiercer-200) &&  bottom_of_window <= (Snowpiercer+100) ){   
-				//내 스크롤값이 스노우 박스(230)보다 크거나 &&  스노우 박스(530)보다 작을때 나타나라
-				
-				console.log("나타났다!");
-                $('.Snow-txt').stop().fadeIn().animate({opacity:"1", top:"-80px"});
-				$('.container').stop().animate({opacity:"0"});
-
-            } else if ( bottom_of_window >= (Snowpiercer+200) ){ 
-				//스노우 박스(630(텍스트 사라짐)
-				$('.Snow-img').stop().fadeIn().animate({opacity:"1"});
-				$('.Snow-txt').stop().animate({opacity:"0"},900).fadeOut();
-				return false
-			}
-    });
-});	
-
-//----------------옥자---------------//
-$(document).ready(function() {
-    $(window).scroll( function(){
-
-			var OKJA = $(".OKJA").offset().top;					//옥자 박스 탑값
-            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
-
+////---------------설국열차---------------//
+//$(document).ready(function() {
+//    $(window).scroll( function(){
+//		    var Snowpiercer = $(".Snowpiercer").offset().top;	//설국열차 박스 탑값
+//            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
+//			
+////			$('.hideme').addClass('SHOWME');
+//
 //			console.log("시작");
-//			console.log("★옥자.offset().top : " + $('.OKJA').offset().top);	//2680
-//			console.log("$(window).scrollTop() : " + $(window).scrollTop());
-//			console.log("끝");			
-
-            if( bottom_of_window <= (OKJA-180) ){
-				// 옥자 박스(2500) 사라짐
-                $('.OKJA-txt').stop().animate({opacity:"0"});
-				$('.OKJA-txt').stop().animate({opacity:"0"});
-            }
-			if( bottom_of_window >= (OKJA-200) &&  bottom_of_window <= (OKJA+100)){
-				//내 스크롤값이 옥자 박스(2480)보다 크거나 &&  스노우 박스(2780)보다 작을때 나타나라
-				console.log("나타났다!");
-                $('.OKJA-txt').stop().fadeIn().animate({opacity:"1", top:"-60px"});
-              	
-            } else if ( bottom_of_window >= (OKJA+110) ){
-				//옥자 박스(2790(텍스트 사라짐)
-				$('.OKJA-img').stop().fadeIn().animate({opacity:"1"},200);
-				$('.OKJA-txt').stop().animate({opacity:"0"},900).fadeOut();
-				return false
-			}
-    });
-	return false
-});	
-
-//-------------------기생충---------------//
-$(document).ready(function() {
-    $(window).scroll( function(){
-
-			var PARASITE = $(".PARASITE").offset().top;			//기생충 박스 탑값
-            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
-
-//			console.log("시작");
-//			console.log("★기생충.offset().top : " + $('.PARASITE').offset().top); //4730
-//			console.log("$(window).scrollTop() : " + $(window).scrollTop());
+//			console.log("★설국열차.offset().top : " + $('.Snowpiercer').offset().top);	//430
+//			console.log("$(window).scrollTop() : " +  bottom_of_window);
 //			console.log("끝");
-
-            if( bottom_of_window <= (PARASITE-450)  ){
-				// 기생충 박스(4280) 사라짐
-                $('.PARA-txt').stop().animate({opacity:"0"});
-				$('.PARA-txt').stop().animate({opacity:"0"});
-            }
-			if( bottom_of_window >= (PARASITE-430) &&  bottom_of_window <= (PARASITE+70) ){
-				//내 스크롤값이 기생충 박스(4300)보다 크거나 &&  기생충 박스(4800)보다 작을때 나타나라
-                $('.PARA-txt').stop().fadeIn().animate({opacity:"1", top:"-100px"});
-              	
-            } else if ( bottom_of_window >= (PARASITE+80)){
-				//기생충 박스(4810(텍스트 사라짐)
-				$('.PARA-img').stop().fadeIn(400).animate({opacity:"1"},300);
-				$('.PARA-txt').stop().animate({opacity:"0"},1000).fadeOut();
-				return false
-			}
-       
-    });
-});	
+//		
+//            if( bottom_of_window <= (Snowpiercer-230)){
+//				// 스노우 박스(200) 사라짐
+//               	$('.Snow-txt').stop().animate({opacity:"0"});
+//				$('.Snow-img').stop().animate({opacity:"0"});
+//            }
+//		
+//			if( bottom_of_window >= (Snowpiercer-200) &&  bottom_of_window <= (Snowpiercer+100) ){   
+//				//내 스크롤값이 스노우 박스(230)보다 크거나 &&  스노우 박스(530)보다 작을때 나타나라
+//				
+//				console.log("나타났다!");
+//                $('.Snow-txt').stop().fadeIn().animate({opacity:"1", top:"-80px"});
+//				$('.container').stop().animate({opacity:"0"});
+//
+//            } else if ( bottom_of_window >= (Snowpiercer+200) ){ 
+//				//스노우 박스(630(텍스트 사라짐)
+//				$('.Snow-img').stop().fadeIn().animate({opacity:"1"});
+//				$('.Snow-txt').stop().animate({opacity:"0"},900).fadeOut();
+//				return false
+//			}
+//    });
+//});	
+//
+////----------------옥자---------------//
+//$(document).ready(function() {
+//    $(window).scroll( function(){
+//
+//			var OKJA = $(".OKJA").offset().top;					//옥자 박스 탑값
+//            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
+//
+////			console.log("시작");
+////			console.log("★옥자.offset().top : " + $('.OKJA').offset().top);	//2680
+////			console.log("$(window).scrollTop() : " + $(window).scrollTop());
+////			console.log("끝");			
+//
+//            if( bottom_of_window <= (OKJA-180) ){
+//				// 옥자 박스(2500) 사라짐
+//                $('.OKJA-txt').stop().animate({opacity:"0"});
+//				$('.OKJA-txt').stop().animate({opacity:"0"});
+//            }
+//			if( bottom_of_window >= (OKJA-200) &&  bottom_of_window <= (OKJA+100)){
+//				//내 스크롤값이 옥자 박스(2480)보다 크거나 &&  스노우 박스(2780)보다 작을때 나타나라
+//				console.log("나타났다!");
+//                $('.OKJA-txt').stop().fadeIn().animate({opacity:"1", top:"-60px"});
+//              	
+//            } else if ( bottom_of_window >= (OKJA+110) ){
+//				//옥자 박스(2790(텍스트 사라짐)
+//				$('.OKJA-img').stop().fadeIn().animate({opacity:"1"},200);
+//				$('.OKJA-txt').stop().animate({opacity:"0"},900).fadeOut();
+//				return false
+//			}
+//    });
+//	return false
+//});	
+//
+////-------------------기생충---------------//
+//$(document).ready(function() {
+//    $(window).scroll( function(){
+//
+//			var PARASITE = $(".PARASITE").offset().top;			//기생충 박스 탑값
+//            var bottom_of_window = $(window).scrollTop();	//윈도우 스크롤
+//
+////			console.log("시작");
+////			console.log("★기생충.offset().top : " + $('.PARASITE').offset().top); //4730
+////			console.log("$(window).scrollTop() : " + $(window).scrollTop());
+////			console.log("끝");
+//
+//            if( bottom_of_window <= (PARASITE-450)  ){
+//				// 기생충 박스(4280) 사라짐
+//                $('.PARA-txt').stop().animate({opacity:"0"});
+//				$('.PARA-txt').stop().animate({opacity:"0"});
+//            }
+//			if( bottom_of_window >= (PARASITE-430) &&  bottom_of_window <= (PARASITE+70) ){
+//				//내 스크롤값이 기생충 박스(4300)보다 크거나 &&  기생충 박스(4800)보다 작을때 나타나라
+//                $('.PARA-txt').stop().fadeIn().animate({opacity:"1", top:"-100px"});
+//              	
+//            } else if ( bottom_of_window >= (PARASITE+80)){
+//				//기생충 박스(4810(텍스트 사라짐)
+//				$('.PARA-img').stop().fadeIn(400).animate({opacity:"1"},300);
+//				$('.PARA-txt').stop().animate({opacity:"0"},1000).fadeOut();
+//				return false
+//			}
+//       
+//    });
+//});	
 //bottom_of_window >= PARASITE && bottom_of_window <= PARASITE+100
 //윈도우 스크롤이 설국열차보다탑(맨위값)보다 크고, 내 스크롤이 설국열차탑값보다 작을때 그 사이 (+100) 사이즈가 되면 나타나라.
 //윈도우 스크롤이 설국열차탑값 300 보다 크면 사라지게하라.
